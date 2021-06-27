f4fb28efdfc7e5fe2d9681ec3eb9639be97e08ec
yuntli <yuntli@paypal.com>
2021-06-27 16:58:05 -0400

    create repo

---
+++ instagram/MainTabBarController.swift
@@ -0,0 +1,38 @@
+//
+//  MainTabBarController.swift
+//  instagram
+//
+//  Created by Yuntao Li on 5/31/21.
+//
+
+import UIKit
+import Firebase
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if Auth.auth().currentUser == nil {
+            //show if not logged in
+            DispatchQueue.main.async {
+                let loginController = LoginController()
+                let navController = UINavigationController(rootViewController: loginController)
+                self.present(navController, animated: true, completion: nil)
+            }
+            
+            return
+        }
+        
+        let layout = UICollectionViewFlowLayout()
+        let userProfileController = UserProfileController(collectionViewLayout: layout)
+        
+        let navController = UINavigationController(rootViewController: userProfileController)
+        
+        navController.tabBarItem.image = UIImage(named: "profile_unselected")
+        navController.tabBarItem.selectedImage = UIImage(named: "profile_selected")
+        
+        tabBar.tintColor = .black
+        
+        viewControllers = [navController, UIViewController()]
+    }
+}
---
+++ instagram/Utils/Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  Extensions.swift
+//  instagram
+//
+//  Created by Yuntao Li on 5/30/21.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    
+//    func someRandomeMethod() {
+//
+//    }
+    
+}
